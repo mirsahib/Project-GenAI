1a14ed5a9ea2a0c36f574320224cfda69701387f
Mir Sahib <mirsahib24@gmail.com>
2024-11-16 10:52:40 +0600

    :hammer: Add web frontend

---
+++ Recommendation_System/src/ui/vite.config.ts
@@ -0,0 +1,16 @@
+import path from "path"
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+    },
+  },
+
+})
